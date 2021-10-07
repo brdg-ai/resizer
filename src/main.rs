6a7611dca5dc46692047f7ae398cc42dd46185bf
David G. Andersen <dga@brdg.ai>
2021-10-06 21:45:33 -0400

    refactor(resizer): improve error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use actix_web::{body, web, App, HttpResponse, HttpServer, get};
+use actix_web::{get, web, App, HttpResponse, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || {
-        App::new()
-            .service(get_image)
-    })
-    .bind("0.0.0.0:8000")?
-    .run()
-    .await
+    HttpServer::new(move || App::new().service(get_image))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
 
 #[get("/{cam}")]
 pub async fn get_image(what: web::Path<u32>) -> actix_web::Result<HttpResponse> {
-    println!("Request");
     let camno = what.into_inner();
     if camno > 9 {
         return Err(actix_web::error::ErrorBadRequest("invalid camera"));
@@ -21,15 +17,18 @@ pub async fn get_image(what: web::Path<u32>) -> actix_web::Result<HttpResponse> 
     let url = format!("http://192.168.10{}.2:8000/", camno);
     let body = reqwest::get(url)
         .await
-        .map_err(|e| actix_web::error::ErrorBadRequest(format!("{}", e)))?
+        .map_err(actix_web::error::ErrorBadRequest)?
         .bytes()
         .await
-        .map_err(|e| actix_web::error::ErrorBadRequest(format!("{}", e)))?;
-    let img = image::io::Reader::new(std::io::Cursor::new(body)).with_guessed_format().expect("Cursor IO never fails").decode().unwrap();
+        .map_err(actix_web::error::ErrorBadRequest)?;
+    let img = image::io::Reader::new(std::io::Cursor::new(body))
+        .with_guessed_format()
+        .expect("Cursor IO never fails")
+        .decode()
+        .map_err(actix_web::error::ErrorInternalServerError)?;
     let img = img.resize(820, 616, image::imageops::FilterType::Nearest);
     let mut b: Vec<u8> = Vec::new();
-    img.write_to(&mut b, image::ImageOutputFormat::Jpeg(50)).unwrap();
-    Ok(HttpResponse::Ok()
-                .content_type("image/jpeg")
-                .body(body::Body::from_slice(&b)))
+    img.write_to(&mut b, image::ImageOutputFormat::Jpeg(50))
+        .map_err(actix_web::error::ErrorInternalServerError)?;
+    Ok(HttpResponse::Ok().content_type("image/jpeg").body(b))
 }
